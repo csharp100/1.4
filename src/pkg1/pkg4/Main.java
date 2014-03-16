4f7577075c111e1a782dfb534e59be7674e0e7cc
csharp100 <clint63012@hotmail.com>
2014-03-16 10:02:29 -0500

    Exercise 1.4 Liang Java 8E Comprehensive Version Initial Commit

---
+++ src/pkg1/pkg4/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Exercise 1.4 from the Liang Java Textbook 8E
+ * Comprehensive Version
+ */
+
+
+package pkg1.pkg4;
+
+/**
+ *
+ * @author Clint Sharp
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("a     a^2     a^3");
+        System.out.println("1     1       1");
+        System.out.println("2     4       8");
+        System.out.println("3     9       27");
+        System.out.println("4     16      64");
+    }
+    
+}
